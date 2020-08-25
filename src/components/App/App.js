bb1bc43dd29a026eb2f463da3c202146cdcd039d
Kameron Ahler <kamahler@gmail.com>
2020-08-24 18:52:06 -0600

    :sweet_potato:

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,19 +4,22 @@ import MemoizedComponent from '../MemoizedComponent/MemoizedComponent'
 
 export default function App() {
   return (
-    <div style={{ display: 'flex', gap: '2rem' }}>
-      <div style={{ flexGrow: 1 }}>
-        <header>
-          <h2>Memoized</h2>
-        </header>
-        <MemoizedComponent />
+    <>
+      <h1>Don't run this on a literal potato</h1>
+      <div style={{ display: 'flex', gap: '2rem' }}>
+        <div style={{ flexGrow: 1 }}>
+          <header>
+            <h2>Memoized</h2>
+          </header>
+          <MemoizedComponent />
+        </div>
+        <div style={{ flexGrow: 1 }}>
+          <header>
+            <h2>Expensive</h2>
+          </header>
+          <ExpensiveComponent />
+        </div>
       </div>
-      <div style={{ flexGrow: 1 }}>
-        <header>
-          <h2>Expensive</h2>
-        </header>
-        <ExpensiveComponent />
-      </div>
-    </div>
+    </>
   )
 }
